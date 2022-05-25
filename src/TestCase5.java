5f4e68cd66bda1ef2d7488b53aab75f5f768bb91
ramiturkmani <ramiturkmani1@gmail.com>
2022-05-25 13:31:36 -0500

    slight revision

--- src/TestCase5.java
+++ src/TestCase5.java
@@ -1,4 +1,5 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.NotFoundException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import utilities.Driver;
@@ -9,6 +10,9 @@ public class TestCase5 {
 
         WebElement emailListInput = driver.findElement(By.xpath("//input[@data-aid='CONTACT_FORM_EMAIL_OPT_IN']"));
         WebElement emailCheckBox = driver.findElement(By.xpath("//label[@data-aid='CONTACT_FORM_EMAIL_OPT_IN']/div"));
+
+        if (!emailListInput.isSelected()) System.out.println("Email check box deselected by default");
+        else throw new NotFoundException("Email check box is selected by default");
 
         emailCheckBox.click();
         System.out.println(emailListInput.isSelected() ?
